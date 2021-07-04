71e539f54a39a81c166a1f63a02493b27bfb9595
Dasol Kim <50112481+Sol-cito@users.noreply.github.com>
2021-07-05 00:04:16 +0900

    Time: 260 ms (68.63%), Space: 14.2 MB (72.55%) - LeetHub

--- count-vowels-permutation/count-vowels-permutation.py
+++ count-vowels-permutation/count-vowels-permutation.py
@@ -1,12 +1,12 @@
 class Solution(object):
     def countVowelPermutation(self, n):
-        dp = [[1] * 5]
+        dp = [1] * 5
         for _ in range(n - 1):
             nDp = [0] * 5
-            nDp[0] = dp[-1][1] + dp[-1][2] + dp[-1][4]
-            nDp[1] = dp[-1][0] + dp[-1][2]
-            nDp[2] = dp[-1][1] + dp[-1][3]
-            nDp[3] = dp[-1][2]
-            nDp[4] = dp[-1][2] + dp[-1][3]
-            dp.append(nDp)
-        return sum(dp[-1]) % (10**9 + 7)
+            nDp[0] = dp[1] + dp[2] + dp[4]
+            nDp[1] = dp[0] + dp[2]
+            nDp[2] = dp[1] + dp[3]
+            nDp[3] = dp[2]
+            nDp[4] = dp[2] + dp[3]
+            dp = nDp
+        return sum(dp) % (10**9 + 7)
