8ddf5612585056f9b479d7b9cdab0aa1db19c7df
Dasol Kim <50112481+Sol-cito@users.noreply.github.com>
2021-07-31 14:52:52 +0900

    Time: 824 ms (35.96%), Space: 22.2 MB (6.97%) - LeetHub

--- 01-matrix/01-matrix.py
+++ 01-matrix/01-matrix.py
@@ -1,17 +1,37 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        ans = [[-1] * len(mat[0]) for _ in range(len(mat))]
-        que = deque([])
+        def topDown(x, y, dp, mat, dir):
+            if dp[x][y] != -1: return dp[x][y]
+            if mat[x][y] == 0:
+                dp[x][y] = 0
+                return 0
+            res = 10 ** 4
+            for dx, dy in dir:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < len(mat) and 0 <= ny < len(mat[0]):
+                    res = min(res, topDown(nx, ny, dp, mat, dir) + 1)
+            dp[x][y] = res
+            return res
+
+        def topDown2(x, y, dp1, dp2, mat, dir):
+            if dp2[x][y] != -1: return dp2[x][y]
+            if mat[x][y] == 0:
+                dp2[x][y] = 0
+                return 0
+            res = dp1[x][y]
+            for dx, dy in dir:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < len(mat) and 0 <= ny < len(mat[0]):
+                    res = min(res, topDown2(nx, ny, dp1, dp2, mat, dir) + 1)
+            dp2[x][y] = res
+            return dp2[x][y]
+
+        dp1 = [[-1] * len(mat[0]) for _ in range(len(mat))]
+        dp2 = [[-1] * len(mat[0]) for _ in range(len(mat))]
         for i in range(len(mat)):
             for j in range(len(mat[0])):
-                if mat[i][j] == 0:
-                    ans[i][j] = 0
-                    que.appendleft([i, j, 1])
-        while que:
-            pop = que.pop()
-            for dx, dy in [1, 0], [0, 1], [-1, 0], [0, -1]:
-                nx, ny = pop[0] + dx, pop[1] + dy
-                if 0 <= nx < len(mat) and 0 <= ny < len(mat[0]) and ans[nx][ny] == -1:
-                    ans[nx][ny] = pop[2]
-                    que.appendleft([nx, ny, pop[2] + 1])
-        return ans
+                topDown(i, j, dp1, mat, [[0, 1], [1, 0]])
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                topDown2(i, j, dp1, dp2, mat, [[0, -1], [-1, 0]])
+        return dp2
