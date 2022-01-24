688cd59b1b22f45bc5d176055cd5dc3d332033bb
Dasol Kim <50112481+Sol-cito@users.noreply.github.com>
2022-01-24 23:27:44 +0900

    Time: 24 ms (97.03%), Space: 14.1 MB (75.02%) - LeetHub

--- 520-detect-capital/520-detect-capital.py
+++ 520-detect-capital/520-detect-capital.py
@@ -1,6 +1,5 @@
 class Solution:
     def detectCapitalUse(self, word: str) -> bool:
         numOfUpper = len(list(filter(lambda x: ord(x) <= 90, word)))
-        print(ord('A'))
         return True if (numOfUpper == len(word) or (numOfUpper == 1 and ord(
             word[0]) <= 90) or numOfUpper == 0) else False
