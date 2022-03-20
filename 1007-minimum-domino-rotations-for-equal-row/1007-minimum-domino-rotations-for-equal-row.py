7b2d84fda07fc261b24393543d71526efdb105b9
Dasol Kim <50112481+Sol-cito@users.noreply.github.com>
2022-03-20 18:57:14 +0900

    Time: 2311 ms (5.02%), Space: 15.1 MB (44.98%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -1,17 +1,17 @@
 class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
         def helper(l1, l2):
-            a = len(l1) + 1
+            res = len(l1) + 1
             for num in range(1, 7):
-                res = 0
+                cnt = 0
                 for i in range(len(l1)):
                     if l1[i] != num and l2[i] != num:
-                        res = len(l1) + 1
+                        cnt = len(l1) + 1
                         break
-                    if l1[i] != num and l2[i] == num:
-                        res +=1
-                a = min(a, res)
-            return a
+                    elif l1[i] != num and l2[i] == num:
+                        cnt +=1
+                res = min(res, cnt)
+            return res
         
         a1, a2 = helper(tops, bottoms), helper(bottoms, tops)
         if a1 == a2 == len(tops) + 1:return -1
