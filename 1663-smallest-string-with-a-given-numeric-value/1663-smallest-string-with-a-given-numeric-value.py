fe5d3cf368600f4e190e965d782a9e62c05e66f4
Dasol Kim <50112481+Sol-cito@users.noreply.github.com>
2022-03-22 20:57:09 +0900

    Time: 1849 ms (16.59%), Space: 17.3 MB (5.07%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
@@ -2,11 +2,11 @@ class Solution:
     def getSmallestString(self, n: int, k: int) -> str:
         k -= n
         arr = [1] * n
-        ans = ""
+        ans = deque([])
         for i in reversed(range(len(arr))):
             if k > 0:
                 r = min(k, 25)
                 arr[i] += r
                 k -= r
-            ans += chr(arr[i] + 96)
-        return ans[::-1]
+            ans.appendleft(chr(arr[i] + 96))
+        return "".join(ans)
