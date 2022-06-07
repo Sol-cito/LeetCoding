6adbe569917bd7700b2937688f5ce471f76c0267
Dasol Kim <50112481+Sol-cito@users.noreply.github.com>
2022-06-07 23:34:29 +0900

    Time: 125 ms (64.01%), Space: 14.4 MB (21.08%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,17 +1,17 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         ans = []
-        leftBalls, leftDist, rightBalls, rightDist = 0, 0, 0, 0
+        leftBalls, leftDistTotal, rightBalls, rightDistTotal = 0, 0, 0, 0
         for i in reversed(range(1, len(boxes))):
             if boxes[i] == '1':
                 rightBalls += 1
-                rightDist += i
+                rightDistTotal += i
         for i in range(len(boxes)):
-            ans.append(leftDist + rightDist)
-            rightDist -= rightBalls
-            if boxes[i] == '1':
-                leftBalls += 1
+            ans.append(leftDistTotal + rightDistTotal)
+            rightDistTotal -= rightBalls
             if i < len(boxes) - 1 and boxes[i + 1] == '1':
                 rightBalls -= 1
-            leftDist += leftBalls
+            if boxes[i] == '1':
+                leftBalls += 1
+            leftDistTotal += leftBalls
         return ans
