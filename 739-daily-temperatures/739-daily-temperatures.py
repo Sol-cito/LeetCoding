350c07b975130ee82e66614a68dde76bc2292106
Dasol Kim <50112481+Sol-cito@users.noreply.github.com>
2022-09-12 17:42:29 +0900

    Time: 1761 ms (54.04%), Space: 25.7 MB (14.76%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -3,11 +3,9 @@ class Solution:
         monostack = []
         ans = [0] * len(temperatures)
         for i in reversed(range(len(temperatures))):
-            while monostack and monostack[-1][0] <= temperatures[i]:
+            while monostack and temperatures[monostack[-1]] <= temperatures[i]:
                 monostack.pop()
-            if not monostack:
-                ans[i] = 0
-            else:
-                ans[i] = monostack[-1][1] - i
-            monostack.append([temperatures[i], i])
+            if monostack:
+                ans[i] = monostack[-1] - i
+            monostack.append(i)
         return ans
