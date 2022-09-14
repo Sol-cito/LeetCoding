8c926dcc8aa12afd05fefa98c35b82418a23a2a1
Dasol Kim <50112481+Sol-cito@users.noreply.github.com>
2022-09-15 02:41:24 +0900

    Time: 2220 ms (7.17%), Space: 84.5 MB (91.90%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -1,14 +1,19 @@
 class Solution:
     def pseudoPalindromicPaths(self, root: Optional[TreeNode]) -> int:
         def dfs(cur, arr):
-            if not cur: return 0
-            arr[cur.val - 1] += 1
-            res = 0
             if not cur.left and not cur.right:
-                if len(list(filter(lambda x: x % 2 != 0, arr))) <= 1: res += 1
-            res += dfs(cur.left, arr.copy())
-            res += dfs(cur.right, arr.copy())
+                return 1 if len(list(filter(lambda x: x % 2 != 0, arr))) <= 1 else 0
+            res = 0
+            if cur.left:
+                arr[cur.left.val - 1] += 1
+                res += dfs(cur.left, arr)
+                arr[cur.left.val - 1] -= 1
+            if cur.right:
+                arr[cur.right.val - 1] += 1
+                res += dfs(cur.right, arr)
+                arr[cur.right.val - 1] -= 1
             return res
 
         arr = [0] * 9
+        arr[root.val - 1] += 1
         return dfs(root, arr)
