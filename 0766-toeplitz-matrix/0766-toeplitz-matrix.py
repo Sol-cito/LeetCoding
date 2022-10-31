1e43e05f57269d838d61bda9e772f388f58660b3
Dasol Kim <50112481+Sol-cito@users.noreply.github.com>
2022-10-31 20:30:09 +0900

    Time: 91 ms (92.91%), Space: 13.9 MB (78.65%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,15 +1,8 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        def helper(a, b):
-            num = matrix[a][b]
-            while 0 <= a < len(matrix) and 0 <= b < len(matrix[0]):
-                if num != matrix[a][b]:return False
-                a +=1
-                b +=1
-            return True
-        
-        for i in range(len(matrix[0]) - 1):
-            if not helper(0, i): return False
+        oneRow = matrix[0]
         for i in range(1, len(matrix)):
-            if not helper(i, 0): return False
+            for j in range(1, len(matrix[0])):
+                if oneRow[j - 1] != matrix[i][j]: return False
+            oneRow = matrix[i]
         return True
